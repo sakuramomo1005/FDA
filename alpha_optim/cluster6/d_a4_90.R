cf2e1ad15d63f1b47b6735747a5da5799f4cb795
Lanqiu "Kate" Yao <ly1192@nyu.edu>
2019-09-17 11:30:09 -0400

    attempt 6

---
+++ alpha_optim/cluster6/d_a4_90.R
@@ -0,0 +1,9 @@
+
+
+theta_angle = 90
+setwd("/gpfs/home/ly1192/FDA_alpha_largesample2")
+n = 500; p = 4; niters = 500
+angles = theta_angle; theta_angle = theta_angle / 180 * pi
+source('functions0915.R')
+names = paste('D4','_a_', angles, '.RData', sep='')
+save(result, file = names)
---
+++ alpha_optim/cluster6/td_a4_0.R
@@ -0,0 +1,9 @@
+
+
+theta_angle = 0
+setwd("/gpfs/home/ly1192/FDA_alpha_largesample2")
+n = 500; p = 4; niters = 500
+angles = theta_angle; theta_angle = theta_angle / 180 * pi
+source('functions_trueD0915.R')
+names = paste('trueD4','_a_', angles, '.RData', sep='')
+save(result, file = names)
